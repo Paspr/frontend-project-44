37da8b6b60eed2bb99a3ebb8fb728c2fd33da19c
Pavel Osminin <osperevod@gmail.com>
2023-05-07 19:02:16 +0500

    fix: remove general game logic from brain-even game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,13 +1,7 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greet from '../src/cli.js';
+import gameLoop from '../src/index.js';
 
-const playerName = greet();
-const rounds = 3;
 const rule = 'Answer "yes" if the number is even, otherwise answer "no"';
-let failure = false;
-
-console.log(rule);
 
 function isEven(number) {
   return number % 2 === 0;
@@ -19,19 +13,4 @@ function generateQuestion() {
   return [number, rightAnswer];
 }
 
-// game loop
-for (let i = 0; i < rounds; i += 1) {
-  const [generatedNumber, rightAnswer] = generateQuestion();
-  console.log(`Question: ${generatedNumber}`);
-  const userAnswer = readlineSync.question('Your answer: ');
-  if (userAnswer !== rightAnswer) {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'`);
-    console.log(`Let's try again, ${playerName}!`);
-    failure = true;
-    break;
-  }
-  console.log('Correct!');
-}
-if (!failure) {
-  console.log(`Congratulations, ${playerName}!`);
-}
+gameLoop(generateQuestion(), rule);
