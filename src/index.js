3086752c0ad3966b37b0c45c0bc14e24ed42e234
Pavel Osminin <osperevod@gmail.com>
2023-05-07 19:01:08 +0500

    feat: move general game logic to src/index.js

---
+++ src/index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import greet from './cli.js';
+
+const rounds = 3;
+let failure = false;
+
+export default function gameLoop(questionFunction, rule) {
+  const playerName = greet();
+  console.log(rule);
+  for (let i = 0; i < rounds; i += 1) {
+    const [generatedNumber, rightAnswer] = questionFunction();
+    console.log(`Question: ${generatedNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== rightAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'`);
+      console.log(`Let's try again, ${playerName}!`);
+      failure = true;
+      break;
+    }
+    console.log('Correct!');
+  }
+  if (!failure) {
+    console.log(`Congratulations, ${playerName}!`);
+  }
+}
