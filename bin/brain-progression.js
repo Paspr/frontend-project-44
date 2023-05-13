46c66792118c656042a0fe4bb13b40729693c9b8
Pavel Osminin <osperevod@gmail.com>
2023-05-13 17:28:41 +0500

    feat: add brain-progression game

---
+++ bin/brain-progression.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import gameLoop from '../src/index.js';
+import { getRandomInt } from '../src/cli.js';
+
+const rule = 'What number is missing in the progression?';
+
+function generateProgression() {
+  const progressionLength = getRandomInt(5, 10);
+  const commonDiffirence = getRandomInt(2, 10);
+  const firstNumber = getRandomInt(0, 100);
+  const missingNumberIndex = getRandomInt(0, progressionLength - 1);
+  const progressionArray = [];
+  progressionArray.push(firstNumber);
+  for (let i = 1; i < progressionLength; i += 1) {
+    progressionArray.push(progressionArray[i - 1] + commonDiffirence);
+  }
+  const missingNumber = progressionArray[missingNumberIndex];
+  progressionArray[missingNumberIndex] = '...';
+  return [progressionArray.toString(), String(missingNumber)];
+}
+
+gameLoop(generateProgression, rule);
